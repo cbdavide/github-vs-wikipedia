cf49d6ce22e5a1f7d7c053c662d44f7d1c91bbd2
David Castelblanco B <david.castelblanco@hotmail.com>
2016-11-04 11:14:16 -0500

    Basic integration with socket.io

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 'use strict'
 
+const http = require('http')
 const express = require('express')
 const ghStream = require('./ghevents')
+const io = require('socket.io')
 
 const app = express()
 
-app.listen(3000, () => {
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+const server = http.createServer(app)
+server.listen(3000, () => {
   console.log('Running on http://localhost:3000')
 })
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+const ioServer = io(server)
+ioServer.on('connection', (client) => {
+  console.log(`${client.id} has connected`)
 })
