778fe3db65a2c30735f96fa4f2bee47bc6a4d0f9
David Castelblanco B <david.castelblanco@hotmail.com>
2016-11-08 22:02:54 -0500

    Basic showcase of the data obtained from GitHub

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,45 @@
 'use strict'
 
 const socket = io()
-const container = document.getElementById('container')
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
+const githubColors = {
+  'PushEvent':          '#4caf50',
+  'CreateEvent':        '#0091ea',
+  'IssuesEvent':        '#f44336',
+  'WatchEvent':         '#ffca28',
+  'DeleteEvent':        '#ff1744',
+  'PullRequestEvent':   '#c6ff00',
+  'IssueCommentEvent':  '#ef5350',
+  'PublicEvent':        '#00e676',
+  'GollumEvent':        '#263238',
+  'ForkEvent':          '#536dfe',
+  'ReleaseEvent':       '#b0bec5',
+  'MemberEvent':        '#4db6ac'
+}
+
+const WIDTH = 500
+const HEIGHT = 300
+
+let ratio = 15
+
+let pos_x = ratio
+let pos_y = ratio
 
 socket.on('gh', (data) => {
-  let element = document.createElement('div')
-  element.textContent = JSON.stringify(data)
+  console.log(data)
 
-  container.appendChild(element)
+  ctx.beginPath()
+  ctx.arc(pos_x, pos_y, ratio, 0, 2 * Math.PI, true)
+
+  if (pos_x < (WIDTH - ratio - 20)) {
+    pos_x += (2 * ratio) + 5
+  } else {
+    pos_x = ratio
+    pos_y += (2 * ratio) + 5
+  }
+
+  ctx.fillStyle = githubColors[data.type]
+  ctx.fill()
 })
